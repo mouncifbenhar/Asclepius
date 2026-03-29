7df71db75ddfb7cdcc82058f8ea7945d3183cffb
monsef benhar <mouncifbenhar7@gmail.com>
2026-03-29 14:27:12 +0100

    add the fillable, casts and relationships in the symptom model

--- app/Models/Symptom.php
+++ app/Models/Symptom.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Symptom extends Model
 {
-     
+     protected $fillable = [
+            'name',
+            'level',
+            'description', 
+            'date_recorded',
+            'notes', 
+            'user_id'
+    ];
+
+    protected $casts = [
+        'notes' => 'array'
+    ];
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
