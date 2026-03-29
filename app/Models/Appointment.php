d89e1f807a9de1061ce3e0af1eb31f2d1958faa9
monsef benhar <mouncifbenhar7@gmail.com>
2026-03-29 14:47:21 +0100

    add the appointment controller with swagger

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -7,5 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Appointment extends Model
 {
-    
+    protected $fillable = [
+           
+            'status',
+            'user_id',
+            'doctor_id',
+            'appointment_date'
+    ];
+
+    protected $casts = [
+         'appointment_date' => 'datetime'
+    ];
+
+
+    public function doctor(){
+        return $this->belongsTo(Doctor::class,'Doctor_id');
+    }
+    public function user(){
+        return $this->belongsTo(User::class,'user_id');
+    }
 }
