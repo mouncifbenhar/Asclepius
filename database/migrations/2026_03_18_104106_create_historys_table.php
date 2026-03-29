3eb38497d04825ed550728871956a1d75bad9bdd
monsef benhar <mouncifbenhar7@gmail.com>
2026-03-29 14:56:30 +0100

    make the historys columns

--- database/migrations/2026_03_18_104106_create_historys_table.php
+++ database/migrations/2026_03_18_104106_create_historys_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('historys', function (Blueprint $table) {
-            
+            $table->id();
+            $table->string('advice');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
